2beb9c12edfbbdce7c3facb9c5f068272f181fde
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2021-10-20 13:15:30 +0530

    Update stack_implimentation.c

--- stack_implimentation.c
+++ stack_implimentation.c
@@ -1,104 +1,80 @@
 #include<stdio.h>
-#include <stdlib.h>
+
 struct stack
 {
-	int size;
+	int arr[100];
 	int top;
-	int *s;
 };
-
-void Display(struct stack st)
+void intlstack(struct stack *s,int si)
 {
- int i;
- for(i=st.top;i>=0;i--)
- printf("%d ",st.s[i]);
- printf("\n");
- 
-}
-
-void push(struct stack *st, int x)
-{
-	if(st->top==st->size-1)
-	  printf("Stack is full ");
-	else
-	{
-	 st->top++;
-	 st->s[st->top]=x;
-}
-}
-
-int pop(struct stack *st)
-{   int x=-1;
-	if(st->top==-1)
-	  printf("Stack is empty ");
-	else
-	{
-     x=st->s[st->top];
-	 st->top--;
-	 
-}
-    return x;
-}
-
-int Peep(struct stack *st)
-{
-	int x;
-		if(st->top==-1)
-	  printf("Stack is empty ");
-	else
-	{
-			x=st->s[st->top];
-			//sp->top--;
-			return x;
-	}	
-}
-
-int main(){
-	
-	struct stack st;
-	int ch,ele;
-	printf("Enter size you want for stack :");
-	scanf("%d",&st.size);
-    st.top=-1;
-	st.s=(int *)malloc(st.size*sizeof(int));
-
-   do
-	{
-		printf("\n 1 For PUSH");
-		printf("\n 2 For POP");
-		printf("\n 3 For PEEP");
-		printf("\n 4 For DISPLAY");
-		printf("\n 5 For QUIT");
-		
-		printf("\n Enter your choice:");
-		scanf("%d", &ch);
-		
-		switch(ch)
-		{
-			case 1: printf("\n Enter the elements to be pushed");
-					scanf("%d", &ele);
-					push(&st,ele);
-					break;
-			case 2: ele = pop(&st);
-					if(ele == -1)
-						printf("\n The stack is empty");
-					else
-						printf("\n The poped element is =%d", ele);
-					break;
-			case 3: ele = Peep(&st);
-					if(ele == -1)
-						printf("\n The stack is empty");
-					else
-						printf("\n The peeped element is =%d", ele);
-					break;
-			case 4: Display(st);
-					break;
-	
-			default: printf("\n Wrong choice! Try again");
-					 break;									
-		} 
-	} while(ch!=st.size); 
-	return 0;
+  s->arr[si];
+  s->top=-1;
 	
 }
-
+void push(struct stack *s,int si,int n)
+{
+	if(s->top ==si-1)
+	{
+		printf("stackoverflow");
+	return;}
+	s->top++;
+	s->arr[s->top]=n;
+}
+int pop(struct stack *s)
+{
+	int num;
+	if(s->top==-1)
+	{
+	printf("stack is empty");
+	return ;
+}
+num=s->arr[s->top];
+s->top--;
+return num;
+}
+void disp( struct stack *s)
+{ int i;
+	for(i=s->top;i>=0;i--){
+		printf("%d",s->arr[i]);
+		s->top++;
+	}
+}
+int main()
+{
+	int ch,size,val,pro;
+	struct stack p;
+	printf("enter size");
+	scanf("%d",&size);
+	int arr[size];
+	intlstack(&p,size);
+	while(1)
+	{
+	 printf("\n\ntSTACK PRIMITIVE OPERATIONS");
+      printf("\n1.PUSH");
+      printf("\n2.POP");
+      printf("\n3.DISPLAY");
+      printf("\n4.QUIT");
+      printf("\nEnter your option : ");
+      scanf("%d",&ch);
+	switch(ch)
+	{
+	case 1:
+	printf("enter number");
+	scanf("%d",&val);
+	push(&p,size,val);
+	break;
+	case 2:
+		pro=pop(&p);
+		printf("element deleted is=%d",pro);
+		break;
+		case 3:
+	    printf("stack inputs");
+     disp(&p);
+     break;
+     case 4:
+     exit(0);
+     	printf("program ended");
+     	break;
+}
+}
+}
